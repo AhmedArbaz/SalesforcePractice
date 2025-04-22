ebdc3a89442d5669e7b6bd0b863ef4de03ea9966
AhmedArbaz <154218937+AhmedArbaz@users.noreply.github.com>
2025-04-22 10:34:18 +0500

    public property boolean

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -3,8 +3,10 @@ import { LightningElement, api } from 'lwc';
 export default class MeetingRoom extends LightningElement {
     @api meetingRoomInfo = {roomName:'A-01', roomCapacity:'12'}
     
-}
-
     // This is our child component where we make @api in js So values come for this component that we made parent for it 
     // the parent has its file name in cabab case (meeting-room-info)
     // NOTE: we create a component that has parent of this component because we add the name of the class is (meeting-room-info), So it is become the child component and that component become parent of this file to pass data between the two components.
+    
+        // Public property boolean
+        @api showRoomsInfo = false;
+}
